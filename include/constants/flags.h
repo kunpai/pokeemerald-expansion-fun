94f5d056fd3b1537224c8eceeb5f98100da04d3d
Kunal Pai <kunpai@ucdavis.edu>
2026-07-10 22:34:02 -0700

    feat: add optional Kanto gym leader bosses (Brock, Misty, Lt. Surge, Erika)

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -61,10 +61,10 @@
 #define FLAG_JOINED_MAGMA     0x27
 #define FLAG_HIDE_AQUA_GRUNT_MERCHANT 0x28
 #define FLAG_HIDE_MAGMA_GRUNT_MERCHANT 0x29
-#define FLAG_UNUSED_0x02A    0x2A // Unused Flag
-#define FLAG_UNUSED_0x02B    0x2B // Unused Flag
-#define FLAG_UNUSED_0x02C    0x2C // Unused Flag
-#define FLAG_UNUSED_0x02D    0x2D // Unused Flag
+#define FLAG_DEFEATED_KANTO_BROCK   0x2A
+#define FLAG_DEFEATED_KANTO_MISTY   0x2B
+#define FLAG_DEFEATED_KANTO_SURGE   0x2C
+#define FLAG_DEFEATED_KANTO_ERIKA   0x2D
 #define FLAG_UNUSED_0x02E    0x2E // Unused Flag
 #define FLAG_UNUSED_0x02F    0x2F // Unused Flag
 #define FLAG_UNUSED_0x030    0x30 // Unused Flag
